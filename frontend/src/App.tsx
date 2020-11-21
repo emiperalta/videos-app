60126e0e4ba50cc11731cbb517e512ae3f0ae36c
EmiPeralta <emilianop96@hotmail.com>
2020-11-21 17:08:32 -0300

    Fixing an error with latest version of typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import VideoForm from './components/Videos/VideoForm';
+import VideoList from './components/Videos/VideoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={VideoList} />
+          <Route path="/addvideo" component={VideoForm} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Videos/VideoForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoForm = () => {
+    return (
+        <div>
+            <h1>VideoForm component</h1>
+        </div>
+    );
+};
+
+export default VideoForm;
